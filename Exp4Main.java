8c67c0f79b5476d570efea07ec5f21c9df527a1e
Sauravkolekar007 <sauravkolekar8007@gmail.com>
2024-12-13 17:37:40 +0530

    Add files via upload

---
+++ Exp4Main.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class Exp4Main {
+	
+	public static void main(String args[])
+	{
+		rectangle r=new rectangle();
+		triangle t=new triangle();
+		r.print_area();
+		t.print_area();
+	}
+
+}
